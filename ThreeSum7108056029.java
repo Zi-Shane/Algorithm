3102be1f40a2aa3a6dca1e85cb050a567ea24de1
zishane <tshuang@smail.nchu.edu.tw>
2020-03-19 17:03:51 +0800

    hash temp1

--- ThreeSum7108056029.java
+++ ThreeSum7108056029.java
@@ -1,11 +1,13 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class ThreeSum7108056029 extends ThreeSum 
 {
 	public static void main(String[] args) 
 	{
 		// int[] a = { 5, 1, -5, 2, 9, 0, 4, -4, 3, 6, -1, -2, -7, -9, -3, -6 }; //7
-		int[] a = { -1, -2, 3, 2, 0 }; //2
+		int[] a = { -1, -2, 3, 2, 0 }; //2 {-2 -1 0 2 3}
 		ThreeSum7108056029 test = new ThreeSum7108056029();
 		System.out.println(test.T_sum(a));
 	}
@@ -13,14 +15,37 @@ public class ThreeSum7108056029 extends ThreeSum
 	@Override
 	public int T_sum(int[] A) 
 	{
+		HashMap<Integer, ArrayList<Integer>> index = new HashMap<Integer, ArrayList<Integer>>();
 		Arrays.sort(A);
 		int N = A.length;
 		int cnt = 0;
-		for (int i = 0; i < N; i++) 
+
+		for (int i = 0; i < A.length; i++) 
+		{
+			int num = A[i];
+			if (index.get(num) == null)
+			{
+				index.put(num, new ArrayList<Integer>());
+				index.get(num).add(i);
+			}
+		}
+		
+		for (int i = 0; i < N; i++)
+		{
 			for (int j = i+1; j < N; j++)
-				if (rank(A, -A[i]-A[j]) > j) 
-					cnt++;
+			{
+				int p = -A[i]-A[j];
+				if (index.get(p) != null) 
+				{
+					cnt += index.get(p).size();
+				}
+
+				// if (rank(A, -A[i]-A[j]) > j) 
+				// 	cnt++;
+			}
+		}
 		return cnt;
+
 	}
 
 	public static int rank(int[] a, int key) 
