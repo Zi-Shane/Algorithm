d97db1332f0ee923f228caff5296e30299247427
zishane <tshuang@smail.nchu.edu.tw>
2020-03-20 18:11:31 +0800

    finish sort

--- ThreeSum7108056029.java
+++ ThreeSum7108056029.java
@@ -14,16 +14,17 @@ public class ThreeSum7108056029 extends ThreeSum
 	@Override
 	public int T_sum(int[] A) 
 	{
-		Arrays.sort(A);
+		mergeSort(A);
 		int N = A.length;
 		int cnt = 0;
 		int j, k, j_plus_k;
 
-		for (int i = 0; i < N-2; i++) {
+		for (int i = 0; i < N-2; i++) 
+		{
 			j = i + 1;
 			k = N - 1;
-
-			while (j < k) {
+			while (j < k) 
+			{
 				j_plus_k = A[j]+A[k];
 				if ( j_plus_k == -A[i]) {
 					cnt++;
@@ -38,8 +39,47 @@ public class ThreeSum7108056029 extends ThreeSum
 		return cnt;
 	}
 
-	public static void sort() {
-		
+	public static void mergeSort(int[] a)
+	{
+		int n = a.length;
+		if (n < 2) {
+			return;
+		}
+		int mid = n / 2;
+		int[] l = new int[mid];
+		int[] r = new int[n - mid];
+
+		for (int i = 0; i < mid; i++) {
+			l[i] = a[i];
+		}
+		for (int i = mid; i < n; i++) {
+			r[i-mid] = a[i];
+		}
+
+		mergeSort(l);
+		mergeSort(r);
+
+		merge(a, l, r, mid, n-mid);
+
+	}
+
+	public static void merge(int[] a, int[] l, int[] r, int left_len, int right_len) 
+	{
+		int i = 0, index_l = 0, index_r = 0;
+		while (index_l < left_len && index_r < right_len) 
+		{
+			if (l[index_l] <= r[index_r]) {
+				a[i++] = l[index_l++];
+			} else {
+				a[i++] = r[index_r++];
+			}
+		}
+		while (index_l < left_len) {
+			a[i++] = l[index_l++];
+		}
+		while (index_r < right_len) {
+			a[i++] = r[index_r++];
+		}
 	}
 
 }
