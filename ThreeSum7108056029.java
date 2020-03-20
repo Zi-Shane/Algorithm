051d283e255e41bf4c1bc13bbade539f3272edb7
zishane <tshuang@smail.nchu.edu.tw>
2020-03-20 14:52:31 +0800

    without sort

--- ThreeSum7108056029.java
+++ ThreeSum7108056029.java
@@ -1,13 +1,12 @@
 import java.util.Arrays;
-import java.util.HashMap;
 
 public class ThreeSum7108056029 extends ThreeSum 
 {
 	public static void main(String[] args) 
 	{
 		// int[] a = { 5, 1, -5, 2, 9, 0, 4, -4, 3, 6, -1, -2, -7, -9, -3, -6 }; //7
-		// int[] a = { -1, -2, 3, 2, 0}; //2 {-2 -1 0 2 3}
-		int[] a = { -1, 0, 1, 2, -4 };
+		int[] a = { -1, -2, 3, 2, 0}; //2 {-2 -1 0 2 3}
+		// int[] a = { -1, 0, 1, 2, -4 };
 		ThreeSum7108056029 test = new ThreeSum7108056029();
 		System.out.println(test.T_sum(a));
 	}
@@ -15,28 +14,32 @@ public class ThreeSum7108056029 extends ThreeSum
 	@Override
 	public int T_sum(int[] A) 
 	{
-		HashMap<Integer, Integer> index = new HashMap<Integer, Integer>();
 		Arrays.sort(A);
 		int N = A.length;
 		int cnt = 0;
+		int j, k, j_plus_k;
 
-		for (int i = 0; i < N; i++) 
-		{
-			if (index.get(A[i]) == null)
-				index.put(A[i], A[i]);
-		}
-		
-		int invert_First2_Sum;
-		for (int i = 0; i < N; i++)
-		{
-			for (int j = i+1; j < N; j++)
-			{
-				invert_First2_Sum = -A[i]-A[j];
-				if (index.get(invert_First2_Sum) != null && invert_First2_Sum > A[j])
+		for (int i = 0; i < N-2; i++) {
+			j = i + 1;
+			k = N - 1;
+
+			while (j < k) {
+				j_plus_k = A[j]+A[k];
+				if ( j_plus_k == -A[i]) {
 					cnt++;
+					j++;
+				} else if ( j_plus_k > -A[i]) {
+					k--;
+				} else {
+					j++;
+				}
 			}
 		}
 		return cnt;
+	}
+
+	public static void sort() {
+		
 	}
 
 }
