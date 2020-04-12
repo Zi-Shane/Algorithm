ef5a48baf975533f4d77d912a0dd785501cf8fe7
zishane <zishane@zishanedeMacBook-Pro.local>
2020-04-12 11:54:00 +0800

    recursive version

--- IslandCounting7108056029.java
+++ IslandCounting7108056029.java
@@ -1,16 +1,15 @@
 
 public class IslandCounting7108056029 extends IslandCounting 
 {
-	private String[] node_map;
-	private int[] A_int;
-	private int[] B_int;
-	private int[] nodes;
-	private int[] sz;
+
 	private int c = 0;
+	private boolean[] marked_A, marked_B;
 	public static void main(String[] args) 
 	{
-		String[] A = {"A", "B", "B", "C", "B", "D", "F", "G"};
-		String[] B = {"E", "E", "C", "D", "D", "E", "H", "H"};
+		// String[] A = {"A", "B", "B", "C", "B", "D", "F", "G"};
+		String[] A = {"A", "B", "E", "C", "E", "B", "D", "F", "G", "L", "E"};
+		// String[] B = {"E", "E", "C", "D", "D", "E", "H", "H"};
+		String[] B = {"E", "C", "C", "D", "A", "D", "E", "H", "H", "O", "O"};
 		IslandCounting7108056029 islandCounting7108056029 = new IslandCounting7108056029();
 		int r = islandCounting7108056029.count(A, B);
 		System.out.println(r);
@@ -20,77 +19,32 @@ public class IslandCounting7108056029 extends IslandCounting
 	public int count(String[] A, String[] B) 
 	{
 		int len = A.length;
-		A_int = new int[len];
-		B_int = new int[len];
-		int map_len = 0;
-		boolean find = false;
-		node_map = new String[len*2];
+		marked_A = new boolean[len];
+		marked_B = new boolean[len];
 
-		for (int i = 0; i < len; i++) 
-		{
-			for (int j = 0; j < map_len; j++) {
-				if (A[i] == node_map[j]) {
-					// old point
-					A_int[i] = j;
-					find = true;
-				}
-			}
-			if (!find) {
-				// add new point
-				A_int[i] = map_len;
-				node_map[map_len++] = A[i];
-			}
-			find = false;
-
-			for (int j = 0; j < map_len; j++) {
-				if (B[i] == node_map[j]) {
-					B_int[i] = j;
-					find = true;
-				}
-			}
-			if (!find) {
-				B_int[i] = map_len;
-				node_map[map_len++] = B[i];
-			}
-			find = false;
-		}
-
-		nodes = new int[map_len];
-		sz = new int[map_len];
-		// init
-		for (int i = 0; i < map_len; i++) {
-			nodes[i] = i;
-			sz[i] = 1;
-		}
-		// do union
 		for (int i = 0; i < len; i++) {
-			union(A_int[i], B_int[i]);
+			if (!marked_A[i]) {
+				find(A, B, B[i], i);
+				c++;
+			}
 		}
-		
-		return map_len - c;
+
+		return c;
 	}
 
-	public int find(int i) {
-		while (i != nodes[i]) {
-			nodes[i] = nodes[nodes[i]];
-			i = nodes[i];
-		}
-		return i;
-	}
-	
-	public void union(int p, int q) {
-		int proot = find(p);
-		int qroot = find(q);
-		if (sz[proot] < sz[qroot]) {
-			nodes[proot] = qroot;
-			sz[qroot] += sz[proot];
-		} else {
-			nodes[qroot] = proot;
-			sz[proot] += sz[qroot];
-		}
-		// nodes[proot] = qroot;
-		if (proot != qroot) {
-			c = c + 1;
+	public void find(String[] A, String[] B, String node, int start_i) 
+	{
+		for (int i = start_i+1; i < A.length; i++) {
+			if (A[i] == node && !marked_A[i]) {
+				marked_A[i] = true;
+				marked_B[i] = true;
+				find(A, B, B[i], start_i);
+			}
+			if (B[i] == node && !marked_B[i]) {
+				marked_A[i] = true;
+				marked_B[i] = true;
+				find(A, B, A[i], start_i);
+			}
 		}
 	}
 }
