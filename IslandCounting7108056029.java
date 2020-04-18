08048604bef5ddf35afb3037f492f4201f6cc2a3
zishane <zishane@zishanedeMacBook-Pro.local>
2020-04-18 19:52:16 +0800

    final islandcounting

--- IslandCounting7108056029.java
+++ IslandCounting7108056029.java
@@ -7,9 +7,9 @@ public class IslandCounting7108056029 extends IslandCounting
 	public static void main(String[] args) 
 	{
 		// String[] A = {"A", "B", "B", "C", "B", "D", "F", "G"};
-		String[] A = {"A", "B", "E", "C", "E", "B", "D", "F", "G", "L", "E"};
+		String[] A = {"A", "B", "E", "C", "E", "B", "D", "F", "G", "L"};
 		// String[] B = {"E", "E", "C", "D", "D", "E", "H", "H"};
-		String[] B = {"E", "C", "C", "D", "A", "D", "E", "H", "H", "O", "O"};
+		String[] B = {"E", "C", "C", "D", "A", "D", "E", "H", "H", "O"};
 		IslandCounting7108056029 islandCounting7108056029 = new IslandCounting7108056029();
 		int r = islandCounting7108056029.count(A, B);
 		System.out.println(r);
@@ -34,16 +34,16 @@ public class IslandCounting7108056029 extends IslandCounting
 
 	public void find(String[] A, String[] B, String node, int start_i) 
 	{
-		for (int i = start_i+1; i < A.length; i++) {
-			if (A[i] == node && !marked_A[i]) {
+		for (int i = 0; i < A.length; i++) {
+			if (A[i] == node && !marked_A[i] && i != start_i) {
 				marked_A[i] = true;
 				marked_B[i] = true;
-				find(A, B, B[i], start_i);
+				find(A, B, B[i], i);
 			}
-			if (B[i] == node && !marked_B[i]) {
+			if (B[i] == node && !marked_B[i] && i != start_i) {
 				marked_A[i] = true;
 				marked_B[i] = true;
-				find(A, B, A[i], start_i);
+				find(A, B, A[i], i);
 			}
 		}
 	}
