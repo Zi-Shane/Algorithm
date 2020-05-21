6b0b6e9ca85c9dd45b0edd3137e3aa0522cfc8d1
zishane <zishane@zishanedeMacBook-Pro.local>
2020-05-21 14:28:40 +0800

    fix SortingArray

--- SortingArray7108056029.java
+++ SortingArray7108056029.java
@@ -1,11 +1,11 @@
 
 public class SortingArray7108056029 extends SortingArray {
 	
-	private static int[] test = {2, 1, 6, 7, 9, 10, 3, 5, 4, 8, 2, -99999, 3, 0};
-	
 	public static void main(String[] args) {
+		int[] test = {2, 1, 6, 7, 9, 1, 1, 1, 10, 3, 5, 4, -99989, 8, 2, -99999, 3, 0};
 		SortingArray7108056029 sortingArray7108056029= new SortingArray7108056029();
 		sortingArray7108056029.sorting(test);
+		// sortingArray7108056029.insertionsort(test, 2, 10);
 		for (int i=0; i< test.length; i++) {
 			System.out.print(test[i] + ", ");
 		}
@@ -13,12 +13,23 @@ public class SortingArray7108056029 extends SortingArray {
 
 	public int[] sorting(int[] A)
 	{
-		quicksort(A, 0, A.length-1);
+		int len = A.length;
+		if (len < 10) {
+			insertionsort(A, 0, len-1);
+		} else {
+			quicksort(A, 0, len-1);
+		}
 		return A;
 	}
 
 	public void quicksort(int[] a, int lo, int hi)
 	{
+		// too small do insertion
+		if (hi-lo+1 <= 10) {
+			insertionsort(a, lo, hi);
+			return;
+		}
+
 		if (lo >= hi) {
 			return;
 		}
@@ -30,19 +41,35 @@ public class SortingArray7108056029 extends SortingArray {
 	public int partition(int[] a, int lo, int hi)
 	{
 		int i = lo - 1; 
-        for(int j = lo; j < hi; j++) { 
-            if(a[j] <= a[hi]) { 
-                i++; 
-                exch(a, i, j); 
-            } 
-        } 
-        exch(a, i+1, hi); 
-        return i+1; 
+		for(int j = lo; j < hi; j++) { 
+			if(a[j] <= a[hi]) { 
+				i++; 
+				exch(a, i, j); 
+			} 
+		} 
+		exch(a, i+1, hi); 
+		return i+1; 
 	}
+
 	public void exch(int[] a, int i, int j) 
 	{
 		int temp = a[i];
 		a[i] = a[j];
 		a[j] = temp;
+	}
+
+	public void insertionsort(int[] a, int lo, int hi) {
+		// int n = hi-lo+1;
+		int key, j;
+		for (int i = lo+1; i < hi+1; ++i) { 
+			key = a[i]; 
+			j = i - 1; 
+
+			while (j >= lo && a[j] > key) { 
+				a[j + 1] = a[j]; 
+				j = j - 1; 
+			} 
+			a[j + 1] = key; 
+		} 
 	}
 }
