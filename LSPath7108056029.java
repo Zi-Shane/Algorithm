499d5e51edaa3cebd1ef13fb313a65850e9bab51
zishane <tshuang@smail.nchu.edu.tw>
2020-05-29 16:23:30 +0800

    init LSPath

---
+++ LSPath7108056029.java
@@ -0,0 +1,18 @@
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class LSPath7108056029 extends LSPath{
+
+	public static void main(String[] args) {
+		int[][] array = {{1, 2}, {1, 3}, {1, 4}, {1, 5}, {1, 11}, {2, 4}, {2, 40}, {2, 106}, {2, 126}, {2, 134}, {3, 9307}, {4, 11}, {4, 45}, {4, 47}, {4, 63}, {5, 311}, {5, 363}, {5, 595}, {5, 658}, {5, 688}};
+
+		LSPath7108056029 lspath = new LSPath7108056029();
+		System.out.print(lspath.Ans(array));
+	}
+
+	public int Ans(int[][] array)
+	{
+		return 0;
+	}
+}
