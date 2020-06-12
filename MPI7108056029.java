0492ff4ca6962766800714303e87666210344c49
zishane <tshuang@smail.nchu.edu.tw>
2020-06-12 17:03:42 +0800

    init hw8

---
+++ MPI7108056029.java
@@ -0,0 +1,12 @@
+
+public class MPI7108056029 extends MPI{
+	public static void main(String[] args) {
+		
+	}
+
+	@Override
+	public int min(int[] array) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
